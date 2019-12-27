aa24497b7c215a01a390f6e3bf8b1acfd1e7262c
zhangandi <zhangandi@360.cn>
2019-12-28 01:38:25 +0800

    feat: 🎸 add sass loader

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import _ from 'lodash';
 import './style.css';
+import './styles.scss';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  element.classList.add('scssColor');
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-      ],
-    }],
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
   },
 };
