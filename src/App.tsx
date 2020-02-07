0618300d799d9daa3f4c8678490ee65d13ca1cbd
andy00614 <33732843@qq.com>
2020-02-07 09:02:18 +0800

    feat: 🎸 write layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,18 @@ import React from 'react';
 import styles from './styles.scss';
 
 const Hello: React.FC<{}> = () => {
-	return <div className={styles.container}>content</div>;
+	return (
+		<div className={styles.container}>
+			<main className={styles.inner}>
+				<header className={styles.header}>
+					<h1 className={styles.name}>Andy Zhang</h1>
+				</header>
+				<p className={styles.desc}>
+					I’m a software engineer. I build things to help people. Currently at Humu,
+					previously manager of React at Facebook and engineer at Khan Academy.
+				</p>
+			</main>
+		</div>
+	);
 };
 export default Hello;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
 import App from './App';
+// import 'reset-css/sass/_reset.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'));
