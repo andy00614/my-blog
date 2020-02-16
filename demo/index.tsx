dea7d9eaaa1853cc5700e571f31ed5c066056cd1
andy00614 <33732843@qq.com>
2020-02-16 18:45:21 +0800

    ssr catelog

--- demo/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import Demo from '../src/index'
-
-ReactDOM.render(<Demo/>,document.getElementById('root'))
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const MyComponent: React.FC = () => {
-	return <div>AndyCompoent</div>;
-};
-export default MyComponent;
