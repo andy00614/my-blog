a0f4576d66f461c4d8f0897c06c57734500e1fa8
andy00614 <33732843@qq.com>
2020-02-16 18:49:24 +0800

    feat: 🎸 adjust webpack common-config

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,38 +1,13 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Webpack = require('webpack');
 
 module.exports = {
-	mode: 'development',
-	entry: {
-		main: path.resolve(__dirname, '../demo/index.tsx')
-	},
-	output: {
-		filename: '[name].[hash:8].js',
-		path: path.resolve(__dirname, '../dist'),
-		libraryTarget: 'umd',
-		libraryExport: 'default',
-	},
 	devtool: 'eval-source-map',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, '../src')
 		},
 		extensions: ['.ts', '.tsx', '.js', '.scss']
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, '../public/index.html')
-		}),
-		new Webpack.HotModuleReplacementPlugin()
-	],
-	devServer: {
-		port: 3000,
-		hot: true,
-		contentBase: '../dist'
 	},
 	module: {
 		rules: [
@@ -77,13 +52,5 @@ module.exports = {
 				loader: 'source-map-loader'
 			}
 		]
-		// When importing a module whose path matches one of the following, just
-		// assume a corresponding global variable exists and use that instead.
-		// This is important because it allows us to avoid bundling all of our
-		// dependencies, which allows browsers to cache those libraries between builds.
-		// externals: {
-		//   react: 'React',
-		//   'react-dom': 'ReactDOM',
-		// },
 	}
 };
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -2,8 +2,19 @@
 const Webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 const WebpackMerge = require('webpack-merge');
+const path = require('path')
 
 module.exports = WebpackMerge(webpackConfig, {
+	mode: 'development',
+	entry: {
+		main: path.resolve(__dirname, '../demo/index.tsx')
+	},
+	output: {
+		filename: '[name].[hash:8].js',
+		path: path.resolve(__dirname, '../dist'),
+		libraryTarget: 'umd',
+		libraryExport: 'default',
+	},
 	devServer: {
 		port: 3000,
 		hot: true,
