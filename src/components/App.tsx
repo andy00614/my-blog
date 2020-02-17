64dffb768876ad3296654bcc00ed8c7537dbd243
andy00614 <33732843@qq.com>
2020-02-17 21:42:55 +0800

    fix: 🐛 article list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import styles from './styles.scss';
+import s from './styles.scss';
+import useStyles from 'isomorphic-style-loader/useStyles'
 
 const Hello: React.FC<{}> = (props) => {
+	useStyles(s)
 	return (
-		<div className={styles.container}>
-			<main className={styles.inner}>
-				<header className={styles.header}>
-					<h1 className={styles.name}>Andy Zhang</h1>
+		<div className={s.container}>
+			<main className={s.inner}>
+				<header className={s.header}>
+					<h1 className={s.name}>Andy</h1>
 				</header>
-				<p className={styles.desc}>Im andy</p>
+				<p className={s.desc}>Everybody wants to be a hero, but nobody wants to start from zero</p>
 				<div>{props.children}</div>
 			</main>
 		</div>
--- src/components/demo/index.tsx
+++ src/components/demo/index.tsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import s from './index.scss'
+import useStyles from 'isomorphic-style-loader/useStyles'
+
+const list = [
+	{
+		time: '2020/01/01',
+		title: 'Fast and maintainable patterns for fetching from a database'
+	},
+	{
+		time: '2020/01/02',
+		title: 'React Conf: “Building a Custom React Renderer”'
+	},
+	{
+		time: '2020/01/03',
+		title: ' Yak shaving and fixing'
+	},
+	{
+		time: '2020/01/04',
+		title: 'Type errors with inference need stacks'
+	}
+];
 
 export default (props) => {
-	return <div>Hello React ssr</div>;
+	useStyles(s)
+	return <div className={s.container}>
+		{
+			list.map(item => (
+				<div key={item.title} className={s.article}>
+					<div className={s.time}>{item.time}</div>
+					<div className={s.title}>{item.title}</div>
+				</div>
+			))
+		}
+	</div>
 };
